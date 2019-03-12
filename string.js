dcf5d8ca06b9bf48bd2c3dfc08147f3450aa7c7a
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-12 14:36:01 +0700

    string template

---
+++ string.js
@@ -0,0 +1,28 @@
+var string = 'This is a string';
+console.log(string);
+
+// Concatenation
+const PI = 3.1416;
+let message = 'PI number is: ' + PI;
+console.log(message);
+
+console.log('Đây là phim \"Iron man\"');
+
+console.log('C:\\Windows');
+
+var stringWithMultiLines = "\
+Hello word \n\
+How are you ? \n\
+";
+console.log(stringWithMultiLines);
+
+// String interpolation
+var name = 'Ngoc Linh Pham';
+var age = 24;
+// Grave accent = `
+console.log(`Hello Mr. ${name}, your age is ${age}`);
+console.log(`
+    Ha ha ha
+       La la la
+Ten ten ten
+`);
