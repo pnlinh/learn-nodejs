f65a20785c4b94eee158d74abc83aa387a6e5969
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-21 09:37:36 +0700

    event arguments

--- first-app/event.js
+++ first-app/event.js
@@ -2,9 +2,9 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on('messageLogged', function () {
-    console.log('Listener called');
-});
+emitter.on('messageLogged', args => console.log('Listener called', args));
 
 // Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id: 1, url: 'http://'});
+
+// Raise: logging (data: message)
